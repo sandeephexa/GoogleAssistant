b2e7948fd1f656f2053664eb6392ab6cc2df149f
sandeephexa <31004610+sandeephexa@users.noreply.github.com>
2017-09-12 17:14:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var apps = express();
 apps.use(bodyParser.json());
-let ApiAiApp = require('actions-on-google').ApiAiAssistant;
+let ApiAiApp = require('actions-on-google').FlightStats;
 
 apps.get("/", function (req, res) {
     res.send("Server is running");
 });
 function callApi(req, res) {
     const app1 = new ApiAiApp({ request: req, response: res });
-    let flightid = app1.getArgument('flightid');
+
     var intent = app1.getIntent();
-    console.log(intent);
+  
     switch (intent) {
         case 'WelcomeIntent':
             app1.ask("hello ! what can i do for you ? test");
