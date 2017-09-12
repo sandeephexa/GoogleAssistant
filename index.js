367f3679673cc00688bcb6bb5488f7dbc3018e3e
sandeephexa <31004610+sandeephexa@users.noreply.github.com>
2017-09-12 16:13:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require("express");
 const app = express();
 var bodyParser = require("body-parser");
-var ApiAiApp = require("actions-on-google").ApiAiApp;
+var ApiAiApp = require("actions-on-google").ApiAiAssistant;
 // const apps = new ApiAiApp({request: request, response: response});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,26 +14,13 @@ function handleIntents(req,res)
     const apps = new ApiAiApp({ request: req, response: res });
     var intent = apps.getIntent();
 
-    if(intent == "WelcomeIntent")
-    {
-        //res.send("Hello ! what can i do for you? ");
-         apps.ask("Hello ! what can i do for you? test");
-    }
-    else if(intent == "FlightStatusByAirport")
-    {
-         apps.ask("Sure ! provide your airport name test");
-       // res.send("Sure ! provide your airport name");
-    }
-    else if(intent == "AirportNameIntent")
-    {
-        apps.ask("tell me the Departure Date along with year. ex: 10 september 2017");
-    }
-    else if(intent == "AirLineNameIntent")
-    {
-        apps.ask("specify the airline name");
-    }
-    else{
-        apps.ask("sorry can you say that again ");
+    switch (intent) {
+        case 'WelcomeIntent':
+            app1.ask("hello ! what can i do for you ? test");
+            break;
+        case 'FlightStatusByAirport':
+            app1.ask("Sure ! give me your airport name. test");
+            break;
     }
     
 }
