62e3011733895b7f45a3b78a4a16e67d701212c4
sandeephexa <31004610+sandeephexa@users.noreply.github.com>
2017-09-13 15:08:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,9 +14,36 @@ function callApi(req, res) {
     const app1 = new ApiAiApp({ request: req, response: res });
     
     var intent = app1.getIntent();
-   if(intent == "WelcomeIntent")
+    if(intent == "WelcomeIntent")
    {
-       app1.ask("Hello ! What can i do for you test?");
+       app1.ask("Hello ! What can i do for you ?");
+   }
+   else if(intent == " FlightStatusByAirport")
+   {
+        app1.ask("provide your airport name test");
+        
+   }
+   else if(intent == " AirportNameIntent")
+   {
+       
+         app1.ask("tell me the Departure Date along with year. example: 10 september 2017 test ");
+
+   }
+   else if(intent == " AirLineNameIntent")
+   {
+        app1.ask("specify the airline name test ");
+   }
+   else if(intent == " FlightTimeIntent")
+   {
+        app1.ask("give the hour of departure from 0 to 23 test");
+   }
+   else if(intent == " FlightListIntent")
+   {
+        app1.ask("status ");
+   }
+   else if(intent == " ThankYouIntent")
+   {
+        app1.ask("Thank you ! Have a nice day. test");
    }
 }
 apps.post("/", function (req, res) {
