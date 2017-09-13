08c3632deb4452e1ca9925327be9088a9cbbee86
sandeephexa <31004610+sandeephexa@users.noreply.github.com>
2017-09-13 13:06:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,29 +3,26 @@ var bodyParser = require('body-parser');
 var apps = express();
 apps.use(bodyParser.json());
 let ApiAiApp = require('actions-on-google').ApiAiAssistant;
+var appId = "6aac18a6";
+var appKey ="40a7e359cb020a07ead5159c2d5d8162";
+var url = "https://api.flightstats.com/flex/flightstatus/rest/v2/json/airport/status/MAA/dep/2017/09/15/15?appId&appKey&utc=false&numHours=1&maxFlights=5"
 
 apps.get("/", function (req, res) {
     res.send("Server is running");
 });
 function callApi(req, res) {
-    console.log("this is response" + res);
     const app1 = new ApiAiApp({ request: req, response: res });
-
+    
     var intent = app1.getIntent();
-    console.log("This is intent name" + intent);
-    switch (intent) {
-        case 'WelcomeIntent':
-            app1.ask("Hello ! test");
-            break;
-        //case 'FlightStatusByAirport':
-        //    app1.ask("Sure ");
-        //   break;
-    }
+   if(intent == "WelcomeIntent")
+   {
+       app1.ask("Hello ! What can i do for you ?");
+   }
 }
 apps.post("/", function (req, res) {
     callApi(req, res);
 
 });
 apps.listen(process.env.PORT || 3000, function () {
-    console.log("testing");
+
 });
