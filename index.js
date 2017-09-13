82dd43e9adc892815599fa308a743826dd4b070c
sandeephexa <31004610+sandeephexa@users.noreply.github.com>
2017-09-13 15:03:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var bodyParser = require('body-parser');
 var apps = express();
 apps.use(bodyParser.json());
 let ApiAiApp = require('actions-on-google').ApiAiAssistant;
-const appId = "6aac18a6";
-const appKey ="40a7e359cb020a07ead5159c2d5d8162";
+var appId = "6aac18a6";
+var appKey ="40a7e359cb020a07ead5159c2d5d8162";
 var url = "https://api.flightstats.com/flex/flightstatus/rest/v2/json/airport/status/MAA/dep/2017/09/15/15?appId&appKey&utc=false&numHours=1&maxFlights=5"
 
 apps.get("/", function (req, res) {
@@ -14,46 +14,9 @@ function callApi(req, res) {
     const app1 = new ApiAiApp({ request: req, response: res });
     
     var intent = app1.getIntent();
-    
-    let flightdate = app1.getArgument('date').split('-');
-    let flightno = app1.getArgument('any');
-    let carrier = app1.getArgument('flight_names');
-
    if(intent == "WelcomeIntent")
    {
-       app1.ask("Hello ! What can i do for you ?");
-   }
-   else if(intent == " FlightStatusByAirport")
-   {
-        app1.ask("provide your airport name ");
-        
-   }
-   else if(intent == " AirportNameIntent")
-   {
-       var airport = app1.getArgument(Airports);
-        console.log(airport);
-         app1.ask("tell me the Departure Date along with year. example: 10 september 2017 ");
-
-   }
-   else if(intent == " AirLineNameIntent")
-   {
-        var date = app1.getArgument(date);
-        console.log(airport);
-        app1.ask("specify the airline name ");
-   }
-   else if(intent == " FlightTimeIntent")
-   {
-        var airline = app1.getArgument(Airlines);
-        console.log(airport);
-        app1.ask("give the hour of departure from 0 to 23");
-   }
-   else if(intent == " FlightListIntent")
-   {
-        app1.ask("status ");
-   }
-   else if(intent == " ThankYouIntent")
-   {
-        app1.ask("Thank you ! Have a nice day.");
+       app1.ask("Hello ! What can i do for you test?");
    }
 }
 apps.post("/", function (req, res) {
