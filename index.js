c9a64af0f6212112e5202399b8c72cbb60401c0d
sandeephexa <31004610+sandeephexa@users.noreply.github.com>
2017-09-14 10:15:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -87,11 +87,12 @@ function callApi(req, res) {
                                 var cityd = destination.city;
                                 var countryd = destination.countryName;
                                 var flightstatuses = fligarriv.flightStatuses[0].status;
-                                app1.ask(`Flight is ${flightstatuses} from ${airports}${countrys} to ${airportd}${countryd}.`);
-                                var scheduledd=fligarriv.flightStatuses[0].operationalTimes.scheduledGateArrival.dateLocal;
-                                var scheduleda=fligarriv.flightStatuses[0].operationalTimes.scheduledGateDeparture.dateLocal;
-                                var estimatedd=fligarriv.flightStatuses[0].operationalTimes.estimatedGateDeparture.dateLocal;
-                                var estimateda=fligarriv.flightStatuses[0].operationalTimes.estimatedGateArrival.dateLocal;
+                                
+                                var departure=fligarriv.flightStatuses[0].operationalTimes.publishedDeparture.dateLocal;
+                                var arrival=fligarriv.flightStatuses[0].operationalTimes.publishedArrival.dateLocal;
+                                app1.ask(`Flight Status : ${flightstatuses} from ${airports}${countrys} to ${airportd}${countryd} departs at ${departure} and arrives at ${arrival} .`);
+                               // var estimatedd=fligarriv.flightStatuses[0].operationalTimes.estimatedGateDeparture.dateLocal;
+                               // var estimateda=fligarriv.flightStatuses[0].operationalTimes.estimatedGateArrival.dateLocal;
                   
 
 
