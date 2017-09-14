ebe5370bd8ed01d635dc49469c81169a27102c60
sandeephexa <31004610+sandeephexa@users.noreply.github.com>
2017-09-14 09:49:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -87,12 +87,12 @@ function callApi(req, res) {
                                 var cityd = destination.city;
                                 var countryd = destination.countryName;
                                 var flightstatuses = fligarriv.flightStatuses[0].status;
-                           
+                                app1.ask(`Flight is ${flightstatuses} from ${airports}${countrys} to ${airportd}${countryd}.`);
                                 var scheduledd=fligarriv.flightStatuses[0].operationalTimes.scheduledGateArrival.dateLocal;
                                 var scheduleda=fligarriv.flightStatuses[0].operationalTimes.scheduledGateDeparture.dateLocal;
                                 var estimatedd=fligarriv.flightStatuses[0].operationalTimes.estimatedGateDeparture.dateLocal;
                                 var estimateda=fligarriv.flightStatuses[0].operationalTimes.estimatedGateArrival.dateLocal;
-                                app1.ask(`Flight is ${flightstatuses} from ${airports}${countrys} to ${airportd}${countryd}.`);
+                  
 
 
                             }
