702734a442bf4bba9773be424c01a068000551de
sandeephexa <31004610+sandeephexa@users.noreply.github.com>
2017-09-15 10:05:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ function callApi(req, res) {
                                 
                                 }
                                  // app1.ask(`Flight is ${flightstatuses} from ${airports}${countrys} to ${airportd}${countryd}.`);
-                                  app1.ask(` flight Status is ${actualStatus}, /n departing  From ${dep}, /n To ${arr}, /n Departs at ${departure}, /n Arrives at ${arrival} .`);
+                                  app1.ask(` flight Status is ${actualStatus}, \n departing  From ${dep}, \n To ${arr}, \n Departs at ${departure}, \n Arrives at ${arrival} .`);
                                
                   
 
