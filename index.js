f1446b8eee54493e5f51304396118adec6ec65bd
sandeephexa <31004610+sandeephexa@users.noreply.github.com>
2017-09-15 18:04:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -138,21 +138,21 @@ function callApi(req, res) {
                     //Working with List
 
                         app1.askWithList(app1.buildRichResponse()
-                            .addSimpleResponse(`* Departs - ${dep} => Arrives ${arr} * \n\n\n* * Departure: ** \n\n${departure}`),
+                            .addSimpleResponse(`Test Response`),
                             // Build a list
                             app1.buildList('Flight Status')
                             // Add the first item to the list
                             .addItems(app1.buildOptionItem('option2')
-                            .setTitle(`${datedep}`)
+                            .setTitle(`Flight status`)
                             .setDescription(`* Departs - ${dep} => Arrives ${arr} * \n\n\n* * Departure: ** \n\n${departure}`)
                             .setImage('https://lh3.googleusercontent.com/K7IBRJz-E1h4gR0wfpcCzwf1MVxV8LXHpqFfKctPdiC54e9GUNTqC_vi_Mhe4KWcB5XnT2ku=w50-h50-e365', 'Departure',50,50)
                             )
                             // Add the second item to the list
                             .addItems(app1.buildOptionItem('option1')
-                            .setTitle(`Arrival`)
+                            .setTitle(`Flight status2`)
                             .setDescription(`* Departs - ${dep} => Arrives ${arr} * \n\n\n* * Departure: ** \n\n${departure}`)
                             .setImage('http://grfx.cstv.com/story-nav/icon-plus3.png', 'Recipe')
-                            ).addItems(app1.buildOptionItem('option3').setTitle(`${sourcecarrier} => ${destcarrier}`)
+                            ).addItems(app1.buildOptionItem('option3').setTitle(`Flight status3`)
                             .setDescription(`* Departs - ${dep} => Arrives ${arr} * \n\n\n* * Departure: ** \n\n${departure}`)
                             )
                         );
